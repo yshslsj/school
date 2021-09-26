cf344862a9dcdfd927ecb051ba9f2ce60cf79c3b
yshslsj <shinghei.yeung@gmail.com>
2021-09-26 15:36:32 +0800

    index page content, class list buttons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import Head from 'next/head'
-import React,{Component} from 'react';
+import {Component} from 'react';
+
+import * as React from 'react';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
+
 import Image from 'next/image'
 import Layout from '../components/layout'
 import styles from './index.module.css'
@@ -35,8 +41,29 @@ class Home extends Component
 					</ul>
 				</div>
 				<div className={homeStyles.cornerDivContent}>
-					<div>
+					<div className={styles.p_contentWrapper}>
 						各班的班照已經可以預覽, 班照共有兩​張：正經版和Q版，請在下方選擇你的班別：
+						<div className={styles.classListWrapper}>
+							<Chip label="1A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="1B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="1C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="2A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="2B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="2C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="3A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="3B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="3C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="4A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="4B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="4C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="4D" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="5A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="5B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="5C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="6A" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="6B" color="primary" variant="outlined" clickable className={styles.classbtn} />
+							<Chip label="6C" color="primary" variant="outlined" clickable className={styles.classbtn} />
+						</div>
 					</div>​
 				</div>
 			</div>
